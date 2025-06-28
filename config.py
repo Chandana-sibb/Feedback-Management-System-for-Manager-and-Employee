dbed1406b0c236122ed74ffb67a27f07f649f9a0
chandana <schandana7899@gmail.com>
2025-06-28 13:27:46 +0530

    added setup instructions,stack and design decisions in readme.md

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///feedback.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'your-jwt-secret'
-    #JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=2)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
