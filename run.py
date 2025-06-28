7b4f5d82b782db217d96c6dd04707f77e7249186
chandana <schandana7899@gmail.com>
2025-06-28 09:47:55 +0530

    removed unused lines

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from flask import  send_from_directory
 from flask_cors import CORS
 from app import create_app
-import webbrowser
+
 
 app = create_app()
 CORS(app)  # Enable CORS for frontend-backend communication
@@ -19,10 +19,6 @@ def index():
 def login_page():
     return send_from_directory(FRONTEND_FOLDER, 'login.html')
 
-# Serve HTML pages from pages/
-@app.route('/pages/<path:filename>')
-def serve_pages(filename):
-    return send_from_directory(os.path.join(FRONTEND_FOLDER, 'pages'), filename)
 
 # Forward specific top-level HTML page routes to pages/
 PAGE_FILES = [
@@ -45,23 +41,7 @@ for page in PAGE_FILES:
         ))(page)
     )
 
-# Serve other static files: login, register, index, etc.
-@app.route('/<path:filename>')
-def serve_static_files(filename):
-    return send_from_directory(FRONTEND_FOLDER, filename)
 
-# Serve frontend assets: JS, components, assets
-@app.route('/js/<path:filename>')
-def serve_js(filename):
-    return send_from_directory(os.path.join(FRONTEND_FOLDER, 'js'), filename)
-
-@app.route('/components/<path:filename>')
-def serve_components(filename):
-    return send_from_directory(os.path.join(FRONTEND_FOLDER, 'components'), filename)
-
-@app.route('/assets/<path:filename>')
-def serve_assets(filename):
-    return send_from_directory(os.path.join(FRONTEND_FOLDER, 'assets'), filename)
 
 if __name__ == '__main__':
     
